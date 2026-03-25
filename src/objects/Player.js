07903a960d8baad5a6a25876e4e6e6d31b8e1b74
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:46:30 +0530

    Add Player class with movement functionality

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,1 +1,25 @@
+export default class Player {
+    constructor(scene, x, y) {
+        this.scene = scene;
+        this.sprite = scene.physics.add.sprite(x, y, 'player');
 
+        this.speed = 200;
+    }
+
+    update(cursors) {
+        this.sprite.setVelocity(0);
+
+        if (cursors.left.isDown) {
+            this.sprite.setVelocityX(-this.speed);
+        }
+        if (cursors.right.isDown) {
+            this.sprite.setVelocityX(this.speed);
+        }
+        if (cursors.up.isDown) {
+            this.sprite.setVelocityY(-this.speed);
+        }
+        if (cursors.down.isDown) {
+            this.sprite.setVelocityY(this.speed);
+        }
+    }
+}
