c4b9c8170b1bcaa018289aca5f04e47339aba29c
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:45:53 +0530

    Add MainScene class with player and controls

--- src/scenes/MainScenes.js
+++ src/scenes/MainScenes.js
@@ -1,1 +1,22 @@
+import Player from "../objects/Player.js";
 
+export default class MainScene extends Phaser.Scene {
+    constructor() {
+        super("MainScene");
+    }
+
+    create() {
+        // Background
+        this.add.image(400, 300, 'background');
+
+        // Player
+        this.player = new Player(this, 400, 300);
+
+        // Controls
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.player.update(this.cursors);
+    }
+}
