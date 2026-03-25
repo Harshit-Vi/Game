3679781e8bf5d1d48f1b018cebf68728d6ab6cf1
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:40:48 +0530

    Initialize Phaser game with scenes

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+import PreloadScene from "./src/scenes/PreloadScene.js";
+import MainScene from "./src/scenes/MainScene.js";
 
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade'
+    },
+    scene: [PreloadScene, MainScene]
+};
+
+new Phaser.Game(config);
