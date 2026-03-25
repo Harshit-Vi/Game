e877ef5264bded5eba78e22a2e5579fe93c88b9d
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:47:20 +0530

    Update DialogueSystem.js

--- src/systems/DialogueSystem.js
+++ src/systems/DialogueSystem.js
@@ -1,1 +1,16 @@
+export default class DialogueSystem {
+    constructor(scene) {
+        this.scene = scene;
+    }
 
+    show(text) {
+        let txt = this.scene.add.text(200, 500, text, {
+            fontSize: '20px',
+            fill: '#ffffff'
+        });
+
+        this.scene.time.delayedCall(2000, () => {
+            txt.destroy();
+        });
+    }
+}
