69b25a6b66e09ec2afff04b5437f1de8282f9989
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:47:03 +0530

    Update BloomSystem.js

--- src/systems/BloomSystem.js
+++ src/systems/BloomSystem.js
@@ -1,1 +1,26 @@
+export default class BloomSystem {
+    constructor(scene) {
+        this.scene = scene;
+        this.active = false;
+    }
 
+    trigger(x, y) {
+        this.active = true;
+
+        for (let i = 0; i < 50; i++) {
+            let flower = this.scene.add.image(
+                x + Phaser.Math.Between(-100, 100),
+                y + Phaser.Math.Between(-100, 100),
+                'flower'
+            );
+
+            flower.setScale(0);
+
+            this.scene.tweens.add({
+                targets: flower,
+                scale: 0.5,
+                duration: 500
+            });
+        }
+    }
+}
