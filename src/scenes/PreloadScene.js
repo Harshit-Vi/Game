38abe2ae8d0fbd301bc8fedd164b3bd4628109aa
𝙃𝙖𝙧𝙨𝙝𝙞𝙩 𝙎𝙞𝙣𝙜𝙝 <harshit.24mim10257@vitbhopal.ac.in>
2026-03-26 02:41:18 +0530

    Add PreloadScene class for asset loading

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -1,1 +1,17 @@
+export default class PreloadScene extends Phaser.Scene {
+    constructor() {
+        super("PreloadScene");
+    }
 
+    preload() {
+        // TEMP placeholders (replace later)
+        this.load.image('player', 'assets/images/player.png');
+        this.load.image('tree', 'assets/images/tree.png');
+        this.load.image('flower', 'assets/images/flower.png');
+        this.load.image('background', 'assets/images/background.png');
+    }
+
+    create() {
+        this.scene.start("MainScene");
+    }
+}
